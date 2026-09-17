f2ab652898bcf32d45acf767ab5c8664dab230ad
Brian McGlinn <brian.mcglinn@gmail.com>
2026-09-17 12:16:36 -0500

    Update anon key after JWT secret rotation

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   // From your Supabase project: Settings > API
   SUPABASE_URL: 'https://supabase.picklevision.net',
-  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzg3NDEzMTM5LCJleHAiOjE5NDUwOTMxMzl9.UYPmuRq3AqdsewwEdya3sEc91Z3VfS12o1cH8IBq2hU',
-};
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzg5NTk5NjM5LCJleHAiOjIxMDQ5NTk2Mzl9.ZEd9nbLayOAX1gbezdh_l2nP02KXcnxyXM8KGS0IDS0',
+ };
